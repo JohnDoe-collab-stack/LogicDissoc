1e3e30b3c60969431d12d177322f238214f958a3
John Doe <Dissociation@outlook.fr>
2025-11-09 19:02:29 +0100

    Add files via upload

---
+++ LogicDissoc.lean
@@ -0,0 +1,8 @@
+import LogicDissoc.BasicSemantics
+import LogicDissoc.AstarConcrete
+import LogicDissoc.Obstruction
+import LogicDissoc.ObstructionGen
+import LogicDissoc.Frequencies
+import LogicDissoc.Thresholds
+import LogicDissoc.Legit
+import LogicDissoc.MetaGodel
